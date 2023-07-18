1a8b93a6a29e79a125dc0a70f43a4ce317bc0039
websivustot <websivustot@gmail.com>
2023-07-18 10:46:07 +0300

    store changed with modern staff configureStore

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,20 @@
-import { combineReducers } from "redux";
-import { createStore, applyMiddleware } from "redux";
+//import { combineReducers } from "redux";
+import { configureStore } from '@reduxjs/toolkit'
+//import { createStore, applyMiddleware } from "redux";
 import reposReducer from "./reposReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk"
+//import { composeWithDevTools } from "redux-devtools-extension";
+//import thunk from "redux-thunk"
 
-const rootReducer = combineReducers({
-    repos: reposReducer,
+// const rootReducer = combineReducers({
+//     repos: reposReducer,
 
-})
+// })
+// Automatically adds the thunk middleware and the Redux DevTools extension
+export const store = configureStore({
+    // Automatically calls `combineReducers`, added thunk
+    reducer: {
+        repos: reposReducer,
+    }
+  })
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+//export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
